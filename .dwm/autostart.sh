cdac6d2c1b95170403cc329f0b07b2647cd0cf26
kaneki-jvk913 <aboutrfass@gmail.com>
2025-03-09 08:18:13 +0000

    add dwm

---
+++ .dwm/autostart.sh
@@ -0,0 +1,22 @@
+
+
+#feh --bg-scale ~/Pictures/wallpaper.jpg &
+
+picom &
+
+#nm-applet &
+
+setxkbmap fr us-azerty & 
+
+/usr/lib/polkit-kde-authentication-agent-1 &
+
+
+xautolock -time 5 -locker "i3lock -i /home/ayoub/Downloads/photo/wallpaper/0-77.png -f -k" &  
+
+nitrogen --restore &
+
+/usr/bin/emacs --daemon &  
+
+slstatus &
+
+# dwmblocks &
